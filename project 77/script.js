00568f3710a7c95845c455455f88aa210027e271
shraddha-2009 <81971413+shraddha-2009@users.noreply.github.com>
2021-07-11 12:59:31 +0530

    Add files via upload

---
+++ project 77/script.js
@@ -0,0 +1,9 @@
+var first_name="Ranbir"
+  var last_name="kapoor"
+  console.log(first_name+last_name)
+  var number1=11
+  var number3=20
+  console.log(number1+number3)
+  console.log(number3-number1)
+console.log(number1*number3)
+console.log(number3/number1)
